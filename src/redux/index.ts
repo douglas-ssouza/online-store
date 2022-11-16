d7316c8601a5a87579cf6a3b7edb11285256b2eb
douglas-ssouza <douglasssouza1994@gmail.com>
2022-11-16 15:10:27 -0300

    feat: adds productsSlice to redux

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,25 +1,10 @@
-import {createSlice, configureStore} from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 
-const productsSlice = createSlice({
-  name: 'products',
-  initialState: {
-    quant: 0,
-    products: [],
-    total: 0,
-  },
-  reducers: {
-    addItem: (state) => {
-      state.quant += 1;
-    },
-    removeItem: (state) => {
-      state.quant -= state.quant === 0 ? 0 : 1;
-    },
-  },
-});
+import productsSlice from './slices/productsSlice';
 
 const store = configureStore({
-  reducer: productsSlice.reducer,
+  reducer: productsSlice,
 });
 
+export type RootState = ReturnType<typeof store.getState>;
 export default store;
-export const {addItem, removeItem} = productsSlice.actions;
